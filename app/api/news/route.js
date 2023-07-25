56872308dd486d32b3f0354380c747aac4ae6070
rocio <rg3113@yahoo.com>
2023-07-25 12:26:52 -0500

    updated headers

--- app/api/news/route.js
+++ app/api/news/route.js
@@ -7,7 +7,7 @@ export  async function GET(request) {
     headers: {
       "X-RapidAPI-Key": process.env.NEWS_KEY,
       "X-RapidAPI-Host": "newsdata2.p.rapidapi.com",
-      "Access-Control-Allow-Origin": origin || "*",
+      "Access-Control-Allow-Origin":  "*",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
     },
