a0eec867d297c1dd8843589bae02b68bb4cbebde
rocio <rg3113@yahoo.com>
2023-07-31 15:20:14 -0500

    add env

--- app/components/Feed.js
+++ app/components/Feed.js
@@ -8,7 +8,7 @@ const shrikhand = Shrikhand({ subsets: ["latin"], weight: ["400"] });
 
 async function fetchNews() {
   try {
-    const response = await fetch("/api/news",
+    const response = await fetch(process.env.GET_NEWS + "/api/news",
   //     {
   //   next: { revalidate: 1 }
   // }
