c71d8b996fb2cd43e36278d331a74fe9ce942eff
rocio <rg3113@yahoo.com>
2023-08-01 22:57:20 -0500

    remove client

--- app/components/Feed.js
+++ app/components/Feed.js
@@ -1,67 +1,59 @@
-"use client"
 import NewsCard from "./newsCard";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { Shrikhand } from "next/font/google";
-import { useState } from "react";
 const shrikhand = Shrikhand({ subsets: ["latin"], weight: ["400"] });
 
-const NewsFeed = async () => {
-const [newsData, setNewsData] = useState([])
+  async function fetchNews() {
+    try {
+      const response = await fetch("/api/news",
+        {
+          cache: "no-store",
+        }
+      );
+      const news =  response.json();
+       return news;
+    } catch (error) {
+      return new Error("no data returned");
+    }
+  }
+async function NewsFeed() {
   const capitalize = (word) => {
     return word[0].toUpperCase() + word.substring(1).toLowerCase();
   };
 
   var newsArr = [];
+  try {
+    const data = await fetchNews();
+    // .then((data) => {
+    console.log("length", data);
+    for (var i = 0; i < data.results.length; i++) {
+      var title = data.results[i].title;
+      var content = data.results[i].content;
+      var creator = data.results[i].creator;
+      var description = data.results[i].description;
+      var pubDate = new Date(data.results[i].pubDate).toString().slice(0, 15);
+      var link = data.results[i].link;
+      var img = data.results[i].image_url;
+      var category = capitalize(data.results[i].category.toString());
 
-  // async function fetchNews() {
-    try {
-      const response = await fetch('/api/news',
-        {
-          cache: "no-store",
-        }
-      );
-      const news = await response.json();
-      setNewsData(news.results);
-      // return news;
-    } catch (error) {
-      return new Error("no data returned");
+      var newsObj = {
+        title: title,
+        content: content,
+        creator: creator,
+        description: description,
+        pubDate: pubDate,
+        link: link,
+        img: img,
+        category: category,
+      };
+      newsArr.push(newsObj);
     }
-  //}
-  
-      try {
-        // const data = await fetchNews();
-          // .then((data) => {
-        console.log("length",newsData)
-      // for (var i = 0; i < data.results.length; i++) {
-      //   var title = data.results[i].title;
-      //   var content = data.results[i].content;
-      //   var creator = data.results[i].creator;
-      //   var description = data.results[i].description;
-      //   var pubDate = new Date(data.results[i].pubDate).toString().slice(0, 15);
-      //   var link = data.results[i].link;
-      //   var img = data.results[i].image_url;
-      //   var category = capitalize(data.results[i].category.toString());
-
-      //   var newsObj = {
-      //     title: title,
-      //     content: content,
-      //     creator: creator,
-      //     description: description,
-      //     pubDate: pubDate,
-      //     link: link,
-      //     img: img,
-      //     category: category,
-      //   };
-      //   newsArr.push(newsObj);
-      // }
-      //  )
+    // )
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-//  }
-
-  
+  //  }
   return (
     <>
       <Grid
@@ -95,10 +87,9 @@ const [newsData, setNewsData] = useState([])
             </Typography>
           </Typography>
         </Grid>
-        
+
         <Grid sx={{ marginTop: 7 }}>
-          {
-            newsData.map((item, index) => {
+          {newsArr.map((item, index) => {
             return (
               <NewsCard
                 key={index}
@@ -108,16 +99,14 @@ const [newsData, setNewsData] = useState([])
                 pubDate={item.pubDate}
                 category={item.category}
                 img={item.img}
-                link={item.link}
-              />
-            )
-            })
-          }
-        </Grid> 
+                link={item.link} />
+            );
+          })}
+        </Grid>
       </Grid>
     </>
   );
-};
+}
 
 export default NewsFeed;
 
