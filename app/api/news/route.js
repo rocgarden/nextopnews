cdbe6055fdb83cc526ab168384aaea9fc6842ac9
rocio <rg3113@yahoo.com>
2023-08-02 13:28:10 -0500

    remove revalidate

--- app/api/news/route.js
+++ app/api/news/route.js
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 
-export const revalidate = 60;
 
 export  async function GET(request) {
   const url = `https://newsdata2.p.rapidapi.com/news?country=us&category=entertainment%2C%20business%2C%20science&language=en`;
